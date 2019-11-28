da3caf8141f1fb3385a062a73747d963c6fa8466
Arthur Denner <arthurdenner7@gmail.com>
2019-11-28 23:15:08 +0100

    Add CSS library

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
 import SemanticDatepicker from "react-semantic-ui-datepickers";
 import "react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css";
 import { DateTime } from "luxon";
